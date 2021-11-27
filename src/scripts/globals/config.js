38da3512dce0fb2236ce2df30157b8505a79900c
= <83763355+mifta17@users.noreply.github.com>
2021-11-27 13:10:43 +0800

    initial commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
+  // CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'paler-resto-v-1',
+  DATABASE_NAME: 'pales-resto-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restorants',
+};
+
+export default CONFIG;
---
+++ src/scripts/views/pages/home.js
@@ -0,0 +1,46 @@
+import RestaurantResource from '../../data/restaurant-source';
+import { createRestaurantItemTemplate } from '../templates/creator-template';
+
+const Home = {
+  async render() {
+    return `
+    <section id="hero">
+        <div class="hero-content">
+            <p class="hero-welcome">Welcome to The</p>
+            <h1 class="hero-title">PALES RESTO</h1>
+            <p class="hero-tag">Find Restaurant with delicious food</p>
+        </div>
+    </section>
+
+    <div class="container">
+      <h2 class="content-title">Explore Restaurant</h2>
+      <div id="loading">
+      
+      </div>
+      <section id="post-list" class="post-list">
+
+      </section>
+    </div>
+    `;
+  },
+
+  async afterRender() {
+    const loading = document.querySelector('#loading');
+    loading.innerHTML = '<div class="loader"></div>';
+
+    try {
+      const restaurants = await RestaurantResource.listRestaurants();
+      const restaurantsContainer = document.querySelector('#post-list');
+      restaurants.forEach((Restaurant) => {
+        restaurantsContainer.innerHTML +=
+          createRestaurantItemTemplate(Restaurant);
+      });
+      loading.style.display = 'none';
+    } catch (err) {
+      loading.style.display = 'none';
+      restaurantsContainer.innerHTML = `Error: ${err}, swipe up to refresh!`;
+    }
+  },
+};
+
+export default Home;
